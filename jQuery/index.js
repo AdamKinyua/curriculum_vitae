d2ae326dc4ad1cfb67c824725d39579f664f6a58
adam_kinyua <85613991+AdamKinyua@users.noreply.github.com>
2022-07-27 03:30:46 -0400

    theSimonGame

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-    $("h1").css("color", "read");
-});
-
 $("h1").css("color", "red");
+
+            //If jQuery CDN and index.js are placed in title region; use the below code to capture the functions expected:
+
+                // $(document).ready(function() {
+                //     $("h1").css("color", "read");
+                // });
+
+//$("button").text("Don't click me");
+$("button").html("<em>Bye</em>");
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+})
